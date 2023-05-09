de8c929128c9502a7d1e5bbd95580c30de5cc93f
hshong1119 <hshong0907@gmail.com>
2023-05-09 17:16:48 +0900

    프로젝트 생성

---
+++ src/main/java/com/hong/ITBloging/ItBlogingApplication.java
@@ -0,0 +1,13 @@
+package com.hong.ITBloging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItBlogingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItBlogingApplication.class, args);
+	}
+
+}
