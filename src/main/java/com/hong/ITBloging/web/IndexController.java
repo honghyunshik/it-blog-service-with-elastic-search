6b141ca9a29124f23615c1ddeae8246a8b733cf9
hshong1119 <hshong0907@gmail.com>
2023-05-10 13:33:17 +0900

    velog 487 블로그 크롤링

--- src/main/java/com/hong/ITBloging/web/IndexController.java
+++ src/main/java/com/hong/ITBloging/web/IndexController.java
@@ -2,6 +2,7 @@ package com.hong.ITBloging.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
@@ -11,8 +12,9 @@ public class IndexController {
     //메인 페이지
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
 
+        //model.addAttribute("posts",)
         return "index";
     }
 }
