139b41e29c3828f60670eca96e8673a23f9da587
hshong1119 <hshong0907@gmail.com>
2023-05-10 16:19:16 +0900

    posts entity 클래스 작성

--- src/main/java/com/hong/ITBloging/domain/posts/Posts.java
+++ src/main/java/com/hong/ITBloging/domain/posts/Posts.java
@@ -1,14 +1,41 @@
 package com.hong.ITBloging.domain.posts;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="postsid")
+    private Long id;
+
+    @Column(name="poststitle", nullable = false)
+    private String title;
+
+    @Column(name="postsregdate",nullable = false)
+    private String regDate;
+
+    @Column(name="postslike",nullable = false)
+    private int like;
+
+    @Column(name="postscontent",nullable = false,length = 2000)
+    private String content;
+
+    @Builder
+    public Posts(String title, String regDate, int like, String content){
+
+        this.title = title;
+        this.regDate = regDate;
+        this.like = like;
+        this.content = content;
+    }
 
 
 }
